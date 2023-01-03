49a22199eab9ef45fdec78be322818a2bd12eeea
Francesco Masala <mail@francescomasala.me>
2023-01-03 16:56:56 +0100

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import React, {useState} from "react";
-import {Dialog} from '@headlessui/react'
-import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline'
+import {Bars3Icon} from '@heroicons/react/24/outline'
 import Link from "next/link";
-import Script from "next/script";
 
 const navigation = [
     {name: 'Home', href: '/'},
-    {name: 'Linkedin', href: ''},
+    {name: 'Services', href: '/services'},
+    {name: 'Company', href: '/company'},
+    {name: 'Linkedin', href: '#'},
     {name: 'Twitter', href: '#'},
 ]
 
@@ -19,7 +18,7 @@ export default function Home() {
         <>
             <Head>
                 <title>HyperBit S.R.L.S.</title>
-                <link rel="shortcut icon" href="/favicon.svg"/>
+                <link rel="shortcut icon" href="/hyperbit-logo-icon-dark.svg"/>
             </Head>
             <div className="isolate bg-white">
                 <div
@@ -35,18 +34,18 @@ export default function Home() {
                             fillOpacity=".3"
                             d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
                         />
-                    <defs>
-                        <linearGradient
-                            id="Gradient1"
-                            x1="1155.49"
-                            x2="-78.208"
-                            y1=".177"
-                            y2="474.645"
-                            gradientUnits="userSpaceOnUse">
-                            <stop stopColor="#9089FC"/>
-                            <stop offset="1" stopColor="#F9A8D4"/>
-                        </linearGradient>
-                    </defs>
+                        <defs>
+                            <linearGradient
+                                id="Gradient1"
+                                x1="1155.49"
+                                x2="-78.208"
+                                y1=".177"
+                                y2="474.645"
+                                gradientUnits="userSpaceOnUse">
+                                <stop stopColor="#9089FC"/>
+                                <stop offset="1" stopColor="#F9A8D4"/>
+                            </linearGradient>
+                        </defs>
                     </svg>
                 </div>
                 <div className="px-6 pt-6 lg:px-8 lg:pt-8">
@@ -96,7 +95,7 @@ export default function Home() {
                                 <div
                                     className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                   <span className="text-gray-600">
-                    Check out our new slack channel {' '}
+                    Check out our new telegram channel {' '}
                       <a href="#" className="font-semibold text-indigo-600">
                       <span className="absolute inset-0" aria-hidden="true"/>
                       Read more <span aria-hidden="true">&rarr;</span>
@@ -109,7 +108,7 @@ export default function Home() {
                                     Home
                                 </h1>
                                 <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                                    Hi! We are currently working on making this site better, if you want you can contact us through one of these buttons
+                                    In theory this is the home page.
                                 </p>
                                 <div className="mt-8 flex gap-x-4 sm:justify-center">
                                     <a
@@ -132,7 +131,8 @@ export default function Home() {
                                     </a>
                                 </div>
                             </div>
-                            <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+                            <div
+                                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                                 <svg
                                     className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
                                     viewBox="0 0 1155 678"
@@ -153,8 +153,8 @@ export default function Home() {
                                             y2="474.645"
                                             gradientUnits="userSpaceOnUse"
                                         >
-                                            <stop stopColor="#9089FC" />
-                                            <stop offset={1} stopColor="#FF80B5" />
+                                            <stop stopColor="#9089FC"/>
+                                            <stop offset={1} stopColor="#FF80B5"/>
                                         </linearGradient>
                                     </defs>
                                 </svg>
@@ -163,35 +163,6 @@ export default function Home() {
                     </div>
                 </div>
             </main>
-            <div className="isolate bg-white">
-                <div
-                    className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-                    <svg
-                        className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-                        viewBox="0 0 1155 678"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-                            fillOpacity=".3"
-                            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                        />
-                        <defs>
-                            <linearGradient
-                                id="Gradient1"
-                                x1="1155.49"
-                                x2="-78.208"
-                                y1=".177"
-                                y2="474.645"
-                                gradientUnits="userSpaceOnUse">
-                                <stop stopColor="#9089FC"/>
-                                <stop offset="1" stopColor="#F9A8D4"/>
-                            </linearGradient>
-                        </defs>
-                    </svg>
-                </div>
-            </div>
         </>
     )
 }
