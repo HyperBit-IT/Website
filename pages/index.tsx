6ce277e37b2858ad57ef43edf1a0627a38d589e9
Francesco Masala <mail@francescomasala.me>
2023-01-03 17:27:51 +0100

    Fixed Mobile menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import React, {useState} from "react";
 import {Bars3Icon} from '@heroicons/react/24/outline'
 import Link from "next/link";
+import {Dialog} from "@headlessui/react";
+import {XMarkIcon} from "@heroicons/react/20/solid";
 
 const navigation = [
     {name: 'Home', href: '/'},
@@ -84,6 +86,51 @@ export default function Home() {
                                 </a>
                             </div>
                         </nav>
+                        <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                            <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
+                                <div className="flex h-9 items-center justify-between">
+                                    <div className="flex">
+                                        <Link href="/" className="-m-1.5 p-1.5">
+                                            <span className="sr-only">HyperBit S.R.L.S.</span>
+                                            <img className="h-8" src="/hyperbit-logo-icon-dark.svg" alt=""/>
+                                        </Link>
+                                    </div>
+                                    <div className="flex">
+                                        <button
+                                            type="button"
+                                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                                            onClick={() => setMobileMenuOpen(false)}
+                                        >
+                                            <span className="sr-only">Close menu</span>
+                                            <XMarkIcon className="h-6 w-6" aria-hidden="true"/>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="mt-6 flow-root">
+                                    <div className="-my-6 divide-y divide-gray-500/10">
+                                        <div className="space-y-2 py-6">
+                                            {navigation.map((item) => (
+                                                <a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            ))}
+                                        </div>
+                                        <div className="py-6">
+                                            <a
+                                                href="#"
+                                                className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                                            >
+                                                Log in
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Dialog.Panel>
+                        </Dialog>
                     </div>
                 </div>
             </div>
