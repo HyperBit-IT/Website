69645cfc6c4dc5f76d11652b0b87c39e2385c934
Francesco Masala <mail@francescomasala.me>
2023-01-06 14:52:21 +0100

    Created code snippets

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,78 +3,41 @@ import React from "react";
 import customFooter from "./_snippets/footer";
 import customHeader from "./_snippets/header";
 import gradient_1 from "./_snippets/gradients";
+import Suggestion from "./_snippets/suggestion";
+import Button from "./_snippets/button";
 
 export default function Home() {
     return (<>
-            <Head>
-                <title>HyperBit S.R.L.S.</title>
-                <link rel="shortcut icon" href="/hyperbit-logo-icon-dark.svg"/>
-            </Head>
-            {customHeader()}
-            <main>
-                <div className="relative px-6 lg:px-8">
-                    <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+        <Head>
+            <title>HyperBit S.R.L.S.</title>
+            <link rel="shortcut icon" href="/hyperbit-logo-icon-dark.svg"/>
+        </Head>
+        {customHeader()}
+        <main>
+            <div className="relative px-6 lg:px-8">
+                <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+                    <div>
+                        {Suggestion("Check out our new telegram channel", "Read more", "https://t.me/HyperBitIT")}
                         <div>
-                            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                                <div
-                                    className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  <span className="text-gray-600">
-                    Check out our new telegram channel {' '}
-                      <a href="https://t.me/HyperBitIT" className="font-semibold text-indigo-600">
-                      <span className="absolute inset-0" aria-hidden="true"/>
-                      Read more <span aria-hidden="true">&rarr;</span>
-                    </a>
-                  </span>
-                                </div>
+                            <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
+                                Home
+                            </h1>
+                            <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                                In theory this is the home page.
+                            </p>
+                            <div className="mt-8 flex gap-x-4 sm:justify-center">
+                                {Button(true, "Send an email ", "mailto://fmasala@hyperbit.it")}
+                                {Button(false, "Contact us on Telegram ", "https://t.me/francescomasala")}
                             </div>
-                            <div>
-                                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
-                                    Home
-                                </h1>
-                                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                                    In theory this is the home page.
-                                </p>
-                                <div className="mt-8 flex gap-x-4 sm:justify-center">
-                                    <a
-                                        href="mailto://fmasala@hyperbit.it"
-                                        className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                                    >
-                                        Send an email
-                                        <span className="text-indigo-200" aria-hidden="true">
-                      &rarr;
-                    </span>
-                                    </a>
-                                    <a
-                                        href="https://t.me/francescomasala"
-                                        className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-                                    >
-                                        Contact us on Telegram
-                                        <span className="text-gray-400" aria-hidden="true">
-                      &rarr;
-                    </span>
-                                    </a>
-                                </div>
-                                <br>
-                                </br>
-                                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                                    <div
-                                        className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  <span className="text-gray-600">
-                    Subscribe to our newsletter {' '}
-                      <a href="https://cdn.forms-content.sg-form.com/a58efb3f-8d04-11ed-b120-fe799afc46d8"
-                         className="font-semibold text-indigo-600">
-                      <span className="absolute inset-0" aria-hidden="true"/>
-                      Press here! <span aria-hidden="true">&rarr;</span>
-                    </a>
-                  </span>
-                                    </div>
-                                </div>
-                            </div>
-                            {gradient_1()}
+                            <br>
+                            </br>
+                            {Suggestion("Subscribe to our newsletter", "Press here!", "https://cdn.forms-content.sg-form.com/a58efb3f-8d04-11ed-b120-fe799afc46d8")}
                         </div>
+                        {gradient_1()}
                     </div>
                 </div>
-            </main>
-            {customFooter()}
-        </>)
+            </div>
+        </main>
+        {customFooter()}
+    </>)
 }
