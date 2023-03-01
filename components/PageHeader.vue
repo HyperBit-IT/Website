ac34da8c0683e79860f0a2eedaf2170d7f188b5e
Francesco Masala <mail@francescomasala.me>
2023-03-01 12:43:05 +0100

    Fixed hamburger menu

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,10 +1,3 @@
-<script>
-
-export default {
-  name: "page-header",
-}
-</script>
-
 <template>
   <nav class="bg-white border-t border-gray-200 px-4 lg:px-6 py-2.5">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
@@ -18,7 +11,7 @@ export default {
         </a>
         <button aria-controls="mobile-menu-2" aria-expanded="false"
                 class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-                data-collapse-toggle="mobile-menu-2" type="button">
+                data-collapse-toggle="mobile-menu-2" type="button" @click="showMenu = !showMenu">
           <span class="sr-only">Open main menu</span>
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path clip-rule="evenodd"
@@ -32,31 +25,31 @@ export default {
           </svg>
         </button>
       </div>
-      <div id="mobile-menu-2" class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
+      <div id="mobile-menu-2" :class="{hidden : !showMenu}" class=" justify-between items-center w-full lg:flex lg:w-auto lg:order-1">
         <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
           <li>
-            <nuxt-link class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+            <nuxt-link @click="showMenu = !showMenu" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                href="/">Home
             </nuxt-link>
           </li>
           <li>
-            <nuxt-link class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+            <nuxt-link @click="showMenu = !showMenu" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                href="/services">Servizi</nuxt-link>
           </li>
           <li>
-            <nuxt-link class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+            <nuxt-link @click="showMenu = !showMenu" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                href="/mission">Missione</nuxt-link>
           </li>
           <li>
-            <nuxt-link class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 "
+            <nuxt-link @click="showMenu = !showMenu" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 "
                href="/about-us">Chi Siamo</nuxt-link>
           </li>
           <li>
-            <nuxt-link class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+            <nuxt-link @click="showMenu = !showMenu" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                href="/contacts">Contatti</nuxt-link>
           </li>
           <li>
-            <nuxt-link class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+            <nuxt-link @click="showMenu = !showMenu" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                href="https://wip.hyperbit.it">Blog</nuxt-link>
           </li>
         </ul>
@@ -64,6 +57,18 @@ export default {
     </div>
   </nav>
 </template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      showMenu: false
+    }
+  },
+  name: "page-header",
+}
+</script>
 
 <style scoped>
 
