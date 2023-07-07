a709611e1a0f300278e1a76dc3be47163d4a0087
Francesco Masala <mail@francescomasala.me>
2023-07-07 21:36:33 +0200

    Added SUSE as partner

--- components/Partners.vue
+++ components/Partners.vue
@@ -2,13 +2,16 @@
     <div class="bg-white py-24 sm:py-16">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">I nostri partner aziendali</h2>
-            <div class="mx-auto mt-10 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-2 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div class="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                 <nuxt-link href="https://www.linode.com/">
-                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/Akamai-Partner-Logo.png" alt="Akamai" width="403" height="126"/>
+                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/Akamai-Partner-Logo.png" alt="Akamai Solutions Partner" width="403" height="126"/>
                 </nuxt-link>
                 <nuxt-link href="https://www.aruba.it">
-                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/aruba-business.png" alt="Aruba Business" width="290" height="83" />
+                    <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/aruba-business.png" alt="Aruba Business Partner" width="290" height="83" />
                 </nuxt-link>
+              <nuxt-link href="https://www.suse.com">
+                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/SUSE_Logo-hor_L_Green-pos_sRGB.png" alt="SUSE Sapphire Partner" width="9000" height="3000" />
+              </nuxt-link>
             </div>
         </div>
     </div>
