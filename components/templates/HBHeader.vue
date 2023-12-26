de7d282c534ec11390f15a2707f5e981cba8bb93
Francesco Masala <fmasala@hyperbit.it>
2023-12-26 20:39:32 +0100

    Removed PopOver

--- components/templates/HBHeader.vue
+++ components/templates/HBHeader.vue
@@ -36,7 +36,6 @@
                 @click="showMenu = !showMenu">{{ $t('home') }}
             </nuxt-link>
           </li>
-          <PopOver/>
           <li>
             <nuxt-link
                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
