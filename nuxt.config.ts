9a03f8ebe6a4ef7f046dbfc8a104e9b4c28be14f
Francesco Masala <fmasala@hyperbit.it>
2024-02-12 21:30:35 +0100

    Updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,24 @@ import {fileURLToPath} from 'url'
 import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-16',
+            title: 'HyperBit SRLs',
+            viewport: 'width=device-width, initial-scale=1',
+            meta: [
+                {property: 'og:title', content: 'HyperBit SRLs'},
+                {property: 'og:description', content: 'HyperBit - We connect the world 🔌'},
+                {property: 'og:type', content: 'website'},
+                {property: 'og:url', content: 'https://hyperbit.it'},
+                {property: 'og:site_name', content: 'HyperBit SRLs'},
+                {property: 'og:locale', content: 'it_IT'},
+            ],
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/HyperBit_Dark_Logo.svg'}
+            ]
+        }
+    },
     colorMode: {preference: "light"},
     devtools: {enabled: true},
     modules: ["@nuxt/ui", '@nuxtjs/plausible'],
@@ -31,18 +49,5 @@ export default defineNuxtConfig({
                 }
             )
         ]
-    },
-    head: {
-        charset: 'utf-16',
-        title: 'HyperBit SRLs',
-        viewport: 'width=device-width, initial-scale=1',
-        meta: [{property: 'og:title', content: 'HyperBit SRLs'}, {
-            property: 'og:description', content: 'HyperBit - We connect the world 🔌'
-        }, {property: 'og:type', content: 'website'}, {
-            property: 'og:url', content: 'https://hyperbit.it'
-        }, {property: 'og:image', content: 'https://hyperbit.it/publicCover.png'}, {
-            property: 'og:site_name', content: 'HyperBit SRLs'
-        }, {property: 'og:locale', content: 'it_IT'},],
-        link: [{rel: 'icon', type: 'image/x-icon', href: '/HyperBit_Dark_Logo.svg'}],
-    },
+    }
 })
